718d341df8e6dea4c24720ad80e0f7b829fb64f4
elibiz443 <ellyambet10@gmail.com>
2021-06-14 05:53:35 +0300

    Added mvc

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  get 'home/index'
+	root 'home#index'
+	resources :users
 
-  root 'home/index'
+	resources :sessions, only: [:new, :create, :destroy]  
+
+	get 'signup', to: 'users#new', as: 'signup'
+  get 'login', to: 'sessions#new', as: 'login'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
 end
